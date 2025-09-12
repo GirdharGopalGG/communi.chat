d89aaff24c3b9bba51848871943691f067f5c27b
Girdhar <greatgmang@gmail.com>
2025-09-12 07:05:44 +0530

    made changes for deployment on sevalla

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,26 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import messageRouter from './routes/message.route.js'
+import path from 'path'
 
 dotenv.config()
 
-const port = process.env.port
+const port = process.env.PORT
 const app = express()
+
+const __dirname = path.resolve()
 
 app.use('/api/auth',authRouter)
 app.use('/api/message',messageRouter)
+
+//for deployment on sevalla
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))    
+
+    app.get("*path",(_,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 app.listen(port,()=>{
     console.log(`listening to port ${port}`)
